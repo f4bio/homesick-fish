c545bf61b91d5216c592b7a2a0c1d59b5f4da6b9
Fabio Tea <ft2011@gmail.com>
2017-06-15 19:40:56 +0200

    save

--- home/.config/fish/conf.d/coreutils.fish
+++ home/.config/fish/conf.d/coreutils.fish
@@ -1,2 +1,4 @@
-set -g -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-set -g -x MANPATH /usr/local/opt/coreutils/libexec/gnuman:(manpath)
+if test -d /usr/local/opt/coreutils/libexec/gnubin
+	set -g -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+	set -g -x MANPATH /usr/local/opt/coreutils/libexec/gnuman:(manpath)
+end
--- home/.config/fish/conf.d/findutils.fish
+++ home/.config/fish/conf.d/findutils.fish
@@ -1,2 +1,4 @@
-set -g -x PATH /usr/local/opt/findutils/libexec/gnubin $PATH
-set -g -x MANPATH /usr/local/opt/findutils/libexec/gnuman:(manpath)
+if test -d /usr/local/opt/findutils/libexec/gnubin
+	set -g -x PATH /usr/local/opt/findutils/libexec/gnubin $PATH
+	set -g -x MANPATH /usr/local/opt/findutils/libexec/gnuman:(manpath)
+end
